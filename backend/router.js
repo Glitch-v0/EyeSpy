b7fc09f9f3f71b1d367cb35094231dd66fa114d4
Nathan Jones <46408104+Glitch-v0@users.noreply.github.com>
2025-02-08 20:09:05 -0600

    Remove rate limiter

--- backend/router.js
+++ backend/router.js
@@ -1,43 +1,26 @@
 import express from "express";
 import controller from "./controller.js";
-import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
-const limiter = rateLimit({
-  windowMs: 2 * 1000, // 1 seconds
-  max: 1, // Allow only 1 request per window
-  message: "Too many requests, please try again later.",
-});
-
-const shortLimiter = rateLimit({
-  windowMs: 0.2 * 1000, // 0.2 seconds
-  max: 1, // Allow only 1 request per window
-  message: "Too many requests, please try again later.",
-});
-
 //get all app pictures from cloudinary
-router.get("/pictures", limiter, async (req, res) => {
+router.get("/pictures", async (req, res) => {
   await controller.sendAllPictureData(req, res);
 });
 
-router.get("/pictures/:name/:screenDimension", limiter, async (req, res) => {
+router.get("/pictures/:name/:screenDimension", async (req, res) => {
   await controller.getPictureURL(req, res);
 });
 
-router.get(
-  "/pictures/:name/guess/:guessCoordinates",
-  shortLimiter,
-  async (req, res) => {
-    await controller.checkGuess(req, res);
-  }
-);
+router.get("/pictures/:name/guess/:guessCoordinates", async (req, res) => {
+  await controller.checkGuess(req, res);
+});
 
-router.get("/scores", limiter, async (req, res) => {
+router.get("/scores", async (req, res) => {
   await controller.getScores(req, res);
 });
 
-router.post("/scores", limiter, async (req, res) => {
+router.post("/scores", async (req, res) => {
   await controller.postScore(req, res);
 });
 
